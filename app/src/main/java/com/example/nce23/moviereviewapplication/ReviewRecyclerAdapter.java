cdda525bfcd302709b71f4dcbbc99bd8f386cdfc
John Doe <dum@joebloggs.com>
2018-08-07 00:12:47 +0800

    Add forgot password layout

--- app/src/main/java/com/example/nce23/moviereviewapplication/ReviewRecyclerAdapter.java
+++ app/src/main/java/com/example/nce23/moviereviewapplication/ReviewRecyclerAdapter.java
@@ -1,9 +1,27 @@
 package com.example.nce23.moviereviewapplication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class ReviewRecyclerAdapter extends RecyclerView.Adapter<ReviewRecyclerAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder{
-        
+
     }
 }
